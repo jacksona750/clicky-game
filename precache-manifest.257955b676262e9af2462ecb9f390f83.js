45623ccbc5fd639a47fa2e6d36df01881edf7ff3
Jacksona750 <jacksona750@gmail.com>
2019-04-29 19:12:29 -0700

    Updates

---
+++ precache-manifest.257955b676262e9af2462ecb9f390f83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e299e4455945eb931eb9cc05a41d230",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "84322b6154fe475e8964",
+    "url": "/clicky-game/static/css/main.6f2d592d.chunk.css"
+  },
+  {
+    "revision": "85a1d543af124641f09c",
+    "url": "/clicky-game/static/js/2.5cb12968.chunk.js"
+  },
+  {
+    "revision": "84322b6154fe475e8964",
+    "url": "/clicky-game/static/js/main.08bf7be5.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
